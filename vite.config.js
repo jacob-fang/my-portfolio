955b342e1cdaec2e781899f0d9ef1fdd3a05dd96
Jacob <aeneasusid@gmail.com>
2024-10-19 04:33:21 +0800

    fix deploy bug

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gh-pages/",
+  base: "/my-portfolio/",
   css: {
     modules: {
       localsConvention: 'camelCase',
